a2c47ba17f4626ba4b7ba7c2f3b8e19702121784
sarahbatiista <sarahbatiista29@gmail.com>
2025-04-09 12:29:30 -0300

    adição de novos conteúdos do dia que faltei

--- app.js
+++ app.js
@@ -1,87 +1,121 @@
-const express = require("express"); //Importa lib do Express
-const sqlite3 = require("sqlite3"); //Importa lib do sqlite3
-const bodyParser = require("body-parser"); //Importa lib do Body-parser
+const bodyParser = require("body-parser"); // Importa o body-parser
+const express = require("express"); // Importa biblioteca do express
+const sqlite3 = require("sqlite3"); // Importa biblioteca sqlite3
 
-const PORT = 8000; //Porta TCP do servidor HTTP da aplicação
+const PORT = 8000; // Porta TCP do servidor HTTP da aplicação
 
-const app = express(); //Instância para uso do Sqlite3
+const app = express(); //Instãncia para uso de express
 
-const db = new sqlite3.Database("user.db"); //Instância para uso de Sqlite3
+const db = new sqlite3.Database("user.db"); // Instância para uso de SQLite3, e usa o arquivo 'user.db'
+// Este método permite enviar comandos SQl em modo 'sequencial'
 db.serialize(() => {
-  //Este método permite enviar comandos SQL em modo 'sequencial'
   db.run(
-    "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, email TEXT, celular TEXT, cpf TEXT, rg TEXT)"
+    `CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT,
+    email TEXT, tel TEXT, cpf TEXT, rg TEXT)`
   );
 });
 
-//_dirname é a variável interna do nodejs que guarda o caminho absoluto do projeto, no SO
-console.log(__dirname + "/static");
+// __dirname é variável interna do nodejs que guarda o caminho absoluto do projeto
+// console.log(__dirname);
 
-//Aqui será acrescentado uma rota "/static" para a pasta _dirname + "/static"
-//O app.use é usado para acrescentar rotas novas para o Express gerenciar e pode usar
-//Middleware para isto, que neste caso é o express.static, que gerencia rotas estáticas
+// Aqui será acrescentado uma rota "/static", parar a pasta __dirname + "/static"
+// O app.use é usado para acrescenter rotas novas para o express gerenciar e poder usar
+// Middleware para isso, que neste caso é o express.static, que gerencia rotas estáticas
 app.use("/static", express.static(__dirname + "/static"));
 
-//Middleware
+// Middleware para processar as requisições do body parameters do cliente
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Configurar EJS como o motor de visualização
 app.set("view engine", "ejs");
 
-//Cria conexão com o banco de dados
-
+// Cria conexão com o banco de dados
 const index =
-  "<a href='/'>Home</a> <a href='/sobre'>Sobre</a> <a href='/login'>Login</a> <a href='/cadastro'>Cadastro</a>";
+  "<a href='/'>Home</a> <a href='/sobre'>Sobre</a> <a href='/login'>login</a> <a href='/cadastro'>Cadastro</a>";
+const sobre = "Você está na página 'sobre'<br><a href='/'>Voltar</a>";
+const login = "Você está na página 'login' <br><a href='/'>Voltar</a>";
+const cadastro = "Você está na página 'cadastro' <br><a href='/'>Voltar</a>";
 
-const cadastro = "Você está na página Cadastro<br><a href='/'>Voltar</a>";
-
-const sobre = "Você está na página Sobre<br><a href='/'>Voltar</a>";
-
-const login = "Você está na página Login<br><a href='/'>Voltar</a>";
-
-/*Método express.get necessita de dois parâmetros
+/* Método express.get necessita de dois parãmetros
 Na ARROW FUNCTION, o primeiro são os dados do servidor (REQUISITION - 'req')
-o segundo são os dados que serão enviados ao cliente (RESULT - 'res') */
+O segundo são os dados que serão enviados ao cliente (RESULT - 'res')
+|*/
 app.get("/", (req, res) => {
-  //Rota raiz do meu servidor, acesse o browser com o endereço http://localhost:8000/
-  /*res.send(index);*/
+  // Rota raiz do meu servidor, acesse o browser com o endereço http://localhost:8000/
+  // res.send(index);
   console.log("GET /index");
-  res.render("index");
+  // res.render(index);
+  res.redirect("cadastro"); // Redireciona para a ROTA cadastro
 });
 
+// Programação de rotas do método GET do HTTP 'app.get()'
 app.get("/sobre", (req, res) => {
   console.log("GET /sobre");
-  //Rota raiz do meu servidor, acesse o browser com o endereço http://localhost:8000/
+  // Rota raiz do meu servidor, acesse o browser com o endereço http://localhost:8000/sobre
   res.render("sobre");
 });
 
+// GET Login
 app.get("/login", (req, res) => {
   console.log("GET /login");
-  // res.send(login);
+  // Rota raiz do meu servidor, acesse o browser com o endereço http://localhost:8000/login
   res.render("login");
 });
 
+// Rota para processar o formulário de login
 app.post("/login", (req, res) => {
-  res.send("Login ainda não implementado.");
+  console.log("POST /login");
+  res.send("Login ainda não implementado");
 });
 
+// GET do cadastro
 app.get("/cadastro", (req, res) => {
   console.log("GET /cadastro");
+  // Rota raiz do meu servidor, acesse o browser com o endereço http://localhost:8000/cadastro
   res.render("cadastro");
 });
 
+// POST do cadastro
 app.post("/cadastro", (req, res) => {
-  req.body
-    ? console.log(JSON.stringify(req.body))
-    : console.log(`Body vazio: $(req.body)`);
-  res.send(
-    res.send(
-      `Bem-vindo usuário ${req.body.username}, seu email é ${req.body.email}`
-    )
-  );
+  console.log("POST /cadastro");
+  // Linha para depurar se esta vindo dados no req.body
+  !req.body
+    ? console.log(`Body vazio: ${req.body}`)
+    : console.log(JSON.stringify(req.body));
+
+  const { username, password, email, tel, cpf, rg } = req.body;
+
+  // Colocar aqui as validações e inclusão no banco de dados do cadastro de usuário
+  // 1. Validar dados do usuário
+
+  // 2. Saber se ele já existe no banco
+  const query =
+    "SELECT * from users WHERE email = ? OR cpf = ? OR rg = ? OR username = ?";
+  db.get(query, [email, cpf, rg, username], (err, row) => {
+    if (err) throw err;
+
+    if (row) {
+      // A variável 'row' irá retornar os dados do banco de dados,
+      // executado através do SQL, variável query
+      res.send("Usuário já cadastrado, refaça o cadastro");
+    } else {
+      // 3. Se usuário não existe no banco, cadastrá-lo
+      const insertQuery =
+        "INSERT INTO users (username, password, email, tel, cpf, rg) VALUES (?,?,?,?,?,?)";
+      db.run(insertQuery, [username, password, email, tel, cpf, rg], (err) => {
+        // Inserir a lógica do INSERT
+        if (err) throw err;
+        res.send("Usuário cadastrado com sucesso");
+      });
+    }
+  });
+
+  // res.send(
+  //   `Bem-vindo usuário: ${req.body.username}, seu email é ${req.body.email}`
+  // );
 });
 
-//app.listen() deve ser o último comando da aplicação (app.js)
+// app.listen() deve ser o último comando da aplicação (app.js)
 app.listen(PORT, () => {
-  console.log(`Servidor sendo executado na porta  ${PORT}!`);
+  console.log(`Servidor sendo executado na porta ${PORT}`);
 });
--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,13 +1,12 @@
 console.log("JS CONECTADO!");
-
 const formulario = document.getElementById("cadastroForm");
 const nome = document.getElementById("nome");
 const email = document.getElementById("email");
-const senha = document.getElementById("password");
-const confirmarSenha = document.getElementById("confirmaPassword");
-const celular = document.getElementById("tel");
-const cpf = document.getElementById("CPF");
-const rg = document.getElementById("RG");
+const senha = document.getElementById("senha");
+const confirmarSenha = document.getElementById("confirmarSenha");
+const celular = document.getElementById("celular");
+const cpf = document.getElementById("cpf");
+const rg = document.getElementById("rg");
 const msgError = document.getElementsByClassName("msgError");
 
 /* ------ FUNÇÃO PARA RENDERIZAR AS DIFERENTES MENSAGENS DE ERRO! ------ */
@@ -78,28 +77,6 @@ function maskPhoneNumber(event) {
 }
 /* --------------------------------------------------------------------- */
 
-/* ------------- FUNÇÃO PARA VERIFICAR FORÇA DA SENHA ------------------ */
-function checkPasswordStrength(senha) {
-  if (!/[a-z]/.test(senha)) {
-    return "A senha deve ter pelo menos uma letra minúscula!";
-  }
-  if (!/[A-Z]/.test(senha)) {
-    return "A senha deve ter pelo menos uma letra maiúscula!";
-  }
-  if (!/[\W_]/.test(senha)) {
-    return "A senha deve ter pelo menos um caractere especial!";
-  }
-  if (!/\d/.test(senha)) {
-    return "A senha deve ter pelo menos um número!";
-  }
-  if (senha.length < 8) {
-    return "A senha deve ter pelo menos 8 caracteres!";
-  }
-
-  return null;
-}
-/* --------------------------------------------------------------------- */
-
 /* ------------- FUNÇÃO PARA VERIFICAR E ENVIAR DADOS ------------------ */
 function fetchDatas(event) {
   event.preventDefault();
@@ -147,7 +124,134 @@ function fetchDatas(event) {
 }
 /* --------------------------------------------------------------------- */
 
-// formulario.addEventListener("submit", fetchDatas);
+/* ------------ FUNÇÃO PARA VERIFICAR A FORÇA DA SENHA ----------------- */
+function checkPasswordStrength(senha) {
+  if (!/[a-z]/.test(senha)) {
+    return "A senha deve ter pelo menos uma letra minúscula!";
+  }
+  if (!/[A-Z]/.test(senha)) {
+    return "A senha deve ter pelo menos uma letra maiúscula!";
+  }
+  if (!/[\W_]/.test(senha)) {
+    return "A senha deve ter pelo menos um caractere especial!";
+  }
+  if (!/\d/.test(senha)) {
+    return "A senha deve ter pelo menos um número!";
+  }
+  if (senha.length < 8) {
+    return "A senha deve ter pelo menos 8 caracteres!";
+  }
+
+  return null;
+}
+/* --------------------------------------------------------------------- */
+
+/* ------------ FUNÇÃO PARA CRIAR "CHUVA" NO FORMULÁRIO ---------------- */
+
+const rainFunction = () => {
+  let rain = document.createElement("span");
+  // let cont_rain = document.getElementsByClassName("container_rain");
+  let cont_rain = document.querySelector(".container_rain");
+  let left = Math.floor(Math.random() * (310 - 65) + 65);
+  let duration = Math.random() * 5;
+
+  rain.classList.add("rain");
+  // cont_rain[0].appendChild(rain);
+  cont_rain.appendChild(rain);
+  rain.style.left = left + "px";
+  rain.style.animationDuration = 1 + duration;
+
+  setTimeout(() => {
+    // cont_rain[0].removeChild(rain);
+    cont_rain.removeChild(rain);
+  }, 1500);
+};
+
+setInterval(() => {
+  rainFunction();
+}, 250);
+
+/* --------------------------------------------------------------------- */
+
+/* ----------------- FUNÇÃO PARA CRIAR MÁSCARA NO CPF ------------------ */
+
+function maskCPF(event) {
+  let cpf = event.target.value;
+
+  if (/[A-Za-zÀ-ÿ]/.test(cpf)) {
+    createDisplayMsgError("O cpf deve conter apenas números!");
+  } else {
+    createDisplayMsgError("");
+  }
+
+  cpf = cpf.replace(/\D/g, ""); // Remove os caracteres não numéricos
+
+  if (cpf.length > 11) {
+    cpf = cpf.substring(0, 11);
+  }
+
+  if (cpf.length > 3) {
+    cpf = `${cpf.substring(0, 3)}.${cpf.substring(3)}`;
+  } else if (cpf.length > 0) {
+    cpf = `${cpf}`;
+  }
+
+  if (cpf.length > 7) {
+    cpf = `${cpf.substring(0, 7)}.${cpf.substring(7)}`;
+  } else if (cpf.length > 0) {
+    cpf = `${cpf}`;
+  }
+
+  if (cpf.length > 11) {
+    cpf = `${cpf.substring(0, 11)}-${cpf.substring(11)}`;
+  } else if (cpf.length > 0) {
+    cpf = `${cpf}`;
+  }
+
+  event.target.value = cpf;
+}
+
+/* --------------------------------------------------------------------- */
+
+/* ----------------- FUNÇÃO PARA CRIAR MÁSCARA NO RG ------------------ */
+
+function maskRG(event) {
+  let rg = event.target.value;
+
+  if (/[A-Za-zÀ-ÿ]/.test(rg)) {
+    createDisplayMsgError("O RG deve conter apenas números!");
+  } else {
+    createDisplayMsgError("");
+  }
+
+  rg = rg.replace(/\D/g, ""); // Remove os caracteres não numéricos
+
+  if (rg.length > 9) {
+    rg = rg.substring(0, 9);
+  }
+
+  if (rg.length > 2) {
+    rg = `${rg.substring(0, 2)}.${rg.substring(2)}`;
+  } else if (rg.length > 0) {
+    rg = `${rg}`;
+  }
+
+  if (rg.length > 6) {
+    rg = `${rg.substring(0, 6)}.${rg.substring(6)}`;
+  } else if (rg.length > 0) {
+    rg = `${rg}`;
+  }
+
+  if (rg.length > 10) {
+    rg = `${rg.substring(0, 10)}-${rg.substring(10)}`;
+  } else if (rg.length > 0) {
+    rg = `${rg}`;
+  }
+
+  event.target.value = rg;
+}
+
+/* --------------------------------------------------------------------- */
 
 nome.addEventListener("input", () => {
   if (nome.value && !checkNome()) {
@@ -175,24 +279,10 @@ senha.addEventListener("input", () => {
   }
 });
 
-function checkPasswordStrength(senha) {
-  if (!/[a-z]/.test(senha)) {
-    return "A senha deve ter pelo menos uma letra minúscula!";
-  }
-  if (!/[A-Z]/.test(senha)) {
-    return "A senha deve ter pelo menos uma letra maiúscula!";
-  }
-  if (!/[\W_]/.test(senha)) {
-    return "A senha deve ter pelo menos um caractere especial!";
-  }
-  if (!/\d/.test(senha)) {
-    return "A senha deve ter pelo menos um número!";
-  }
-  if (senha.length < 8) {
-    return "A senha deve ter pelo menos 8 caracteres!";
-  }
-
-  return null;
-}
+formulario.addEventListener("submit", fetchDatas);
 
 celular.addEventListener("input", maskPhoneNumber);
+
+cpf.addEventListener("input", maskCPF);
+
+rg.addEventListener("input", maskRG);
